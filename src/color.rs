6768ca1fb84e1fd9e6bf68393007a5b0899ee89a
Ethan Dobrowolski <ethan.dobrowolski@gmail.com>
2024-01-02 23:36:36 -0500

    Bound colors from 0 to 1, update tests, use new sample image

--- src/color.rs
+++ src/color.rs
@@ -2,13 +2,54 @@ use std::fmt;
 use std::ops;
 
 #[derive(Debug, Clone, Copy)]
+enum ChannelKind {
+    Red,
+    Blue,
+    Green,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Channel {
+    val: f64,
+    kind: ChannelKind,
+}
+
+impl Channel {
+    // Clamp instead of using Option
+    pub fn new(val: f64, kind: ChannelKind) -> Channel {
+        if 0.0 <= val && val < 1.0 {
+            Channel { val, kind }
+        } else if val < 0.0 {
+            Channel { val: 0.0, kind }
+        } else {
+            Channel {
+                val: 1.0 - f64::EPSILON,
+                kind,
+            }
+        }
+    }
+
+    pub fn to_byte(&self) -> u8 {
+        let val = f64::round(self.val * 256.0) as u16;
+        let val = if val > 255 { 255 } else { val };
+        val as u8
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
 pub struct Color {
-    e: [f64; 3],
+    e: [Channel; 3],
 }
 
 impl Color {
     pub fn new(r: f64, g: f64, b: f64) -> Color {
-        Color { e: [r, g, b] }
+        Color {
+            e: [
+                Channel::new(r, ChannelKind::Red),
+                Channel::new(g, ChannelKind::Green),
+                Channel::new(b, ChannelKind::Blue),
+            ],
+        }
     }
 
     pub fn r(&self) -> f64 {
@@ -22,6 +63,14 @@ impl Color {
     pub fn b(&self) -> f64 {
         self[2]
     }
+
+    pub fn to_bytes(&self) -> [u8; 3] {
+        [
+            self.e[0].to_byte(),
+            self.e[1].to_byte(),
+            self.e[2].to_byte(),
+        ]
+    }
 }
 
 impl fmt::Display for Color {
@@ -33,13 +82,13 @@ impl fmt::Display for Color {
 impl ops::Index<usize> for Color {
     type Output = f64;
     fn index<'a>(&'a self, i: usize) -> &'a f64 {
-        &self.e[i]
+        &self.e[i].val
     }
 }
 
 impl ops::IndexMut<usize> for Color {
     fn index_mut<'a>(&'a mut self, i: usize) -> &'a mut f64 {
-        &mut self.e[i]
+        &mut self.e[i].val
     }
 }
 
@@ -133,82 +182,94 @@ mod tests {
     use super::Color;
 
     fn to_p7(f: f64) -> u64 {
-        f64::round(f * 1000000.0) as u64
+        f64::round(f * 10000000.0) as u64
+    }
+
+    fn to_3p7(c: [f64; 3]) -> [u64; 3] {
+        [to_p7(c[0]), to_p7(c[1]), to_p7(c[2])]
     }
 
     #[test]
     fn color_general() {
-        let c = Color::new(1.0, 2.0, 3.0);
-        let d = Color::new(4.0, 5.0, 6.0);
+        let c = Color::new(0.1, 0.2, 0.3);
+        let d = Color::new(0.4, 0.5, 0.6);
 
-        assert_eq!(c[0], 1.0);
-        assert_eq!(c[1], 2.0);
-        assert_eq!(c[2], 3.0);
+        assert_eq!(to_p7(c[0]), to_p7(0.1));
+        assert_eq!(to_p7(c[1]), to_p7(0.2));
+        assert_eq!(to_p7(c[2]), to_p7(0.3));
+        assert_eq!(c.to_bytes(), [26, 51, 77]);
 
-        assert_eq!(d[0], 4.0);
-        assert_eq!(d[1], 5.0);
-        assert_eq!(d[2], 6.0);
+        assert_eq!(to_p7(d[0]), to_p7(0.4));
+        assert_eq!(to_p7(d[1]), to_p7(0.5));
+        assert_eq!(to_p7(d[2]), to_p7(0.6));
+        assert_eq!(d.to_bytes(), [102, 128, 154]);
 
         let u = c + d;
-        assert_eq!([u[0], u[1], u[2]], [5.0, 7.0, 9.0]);
+        assert_eq!(to_3p7([u[0], u[1], u[2]]), to_3p7([0.5, 0.7, 0.9]));
         let u = d + c;
-        assert_eq!([u[0], u[1], u[2]], [5.0, 7.0, 9.0]);
+        assert_eq!(to_3p7([u[0], u[1], u[2]]), to_3p7([0.5, 0.7, 0.9]));
         let mut u = c;
-        assert_eq!([u[0], u[1], u[2]], [1.0, 2.0, 3.0]);
+        assert_eq!(to_3p7([u[0], u[1], u[2]]), to_3p7([0.1, 0.2, 0.3]));
         u += d;
-        assert_eq!([u[0], u[1], u[2]], [5.0, 7.0, 9.0]);
+        assert_eq!(to_3p7([u[0], u[1], u[2]]), to_3p7([0.5, 0.7, 0.9]));
         u += c;
-        assert_eq!([u[0], u[1], u[2]], [6.0, 9.0, 12.0]);
+        assert_eq!(
+            to_3p7([u[0], u[1], u[2]]),
+            to_3p7([0.6, 0.9, 1.0 - f64::EPSILON])
+        );
 
         let u = c - d;
-        assert_eq!([u[0], u[1], u[2]], [-3.0, -3.0, -3.0]);
+        assert_eq!(to_3p7([u[0], u[1], u[2]]), to_3p7([0.0, 0.0, 0.0]));
         let u = d - c;
-        assert_eq!([u[0], u[1], u[2]], [3.0, 3.0, 3.0]);
+        assert_eq!(to_3p7([u[0], u[1], u[2]]), to_3p7([0.3, 0.3, 0.3]));
         let mut u = Color::new(0.0, 0.0, 0.0);
         u += c + d;
-        assert_eq!([u[0], u[1], u[2]], [5.0, 7.0, 9.0]);
+        assert_eq!(to_3p7([u[0], u[1], u[2]]), to_3p7([0.5, 0.7, 0.9]));
         u -= c;
-        assert_eq!([u[0], u[1], u[2]], [4.0, 5.0, 6.0]);
+        assert_eq!(to_3p7([u[0], u[1], u[2]]), to_3p7([0.4, 0.5, 0.6]));
 
         let u = c * d;
-        assert_eq!([u[0], u[1], u[2]], [4.0, 10.0, 18.0]);
+        assert_eq!(to_3p7([u[0], u[1], u[2]]), to_3p7([0.04, 0.1, 0.18]));
         let u = d * c;
-        assert_eq!([u[0], u[1], u[2]], [4.0, 10.0, 18.0]);
+        assert_eq!(to_3p7([u[0], u[1], u[2]]), to_3p7([0.04, 0.1, 0.18]));
         let u = c / d;
         assert_eq!(
-            [to_p7(u[0]), to_p7(u[1]), to_p7(u[2])],
-            [to_p7(1.0 / 4.0), to_p7(2.0 / 5.0), to_p7(3.0 / 6.0)]
+            to_3p7([u[0], u[1], u[2]]),
+            to_3p7([0.1 / 0.4, 0.2 / 0.5, 0.3 / 0.6])
         );
         let u = d / c;
         assert_eq!(
-            [to_p7(u[0]), to_p7(u[1]), to_p7(u[2])],
-            [to_p7(4.0 / 1.0), to_p7(5.0 / 2.0), to_p7(6.0 / 3.0)]
+            to_3p7([u[0], u[1], u[2]]),
+            to_3p7([1.0 - f64::EPSILON, 1.0 - f64::EPSILON, 1.0 - f64::EPSILON])
         );
         let mut u = c;
         u *= d;
-        assert_eq!([u[0], u[1], u[2]], [4.0, 10.0, 18.0]);
+        assert_eq!(to_3p7([u[0], u[1], u[2]]), to_3p7([0.04, 0.1, 0.18]));
         u /= d;
-        assert_eq!([u[0], u[1], u[2]], [1.0, 2.0, 3.0]);
+        assert_eq!(to_3p7([u[0], u[1], u[2]]), to_3p7([0.1, 0.2, 0.3]));
         u /= d;
         assert_eq!(
             [to_p7(u[0]), to_p7(u[1]), to_p7(u[2])],
             [to_p7(1.0 / 4.0), to_p7(2.0 / 5.0), to_p7(3.0 / 6.0)]
         );
 
-        let u = 5.0 * c;
-        assert_eq!([u[0], u[1], u[2]], [5.0, 10.0, 15.0]);
-        let u = c * 5.0;
-        assert_eq!([u[0], u[1], u[2]], [5.0, 10.0, 15.0]);
-        let mut u = 5.0 * c;
-        assert_eq!([u[0], u[1], u[2]], [5.0, 10.0, 15.0]);
-        u *= 2.0;
-        assert_eq!([u[0], u[1], u[2]], [10.0, 20.0, 30.0]);
+        let u = 2.0 * c;
+        assert_eq!(to_3p7([u[0], u[1], u[2]]), to_3p7([0.2, 0.4, 0.6]));
+        let u = c * 2.0;
+        assert_eq!(to_3p7([u[0], u[1], u[2]]), to_3p7([0.2, 0.4, 0.6]));
+        let mut u = 3.0 * c;
+        assert_eq!(to_3p7([u[0], u[1], u[2]]), to_3p7([0.3, 0.6, 0.9]));
+        u *= 1.1;
+        assert_eq!(to_3p7([u[0], u[1], u[2]]), to_3p7([0.33, 0.66, 0.99]));
         u /= 2.0;
-        assert_eq!([u[0], u[1], u[2]], [5.0, 10.0, 15.0]);
+        assert_eq!(
+            to_3p7([u[0], u[1], u[2]]),
+            to_3p7([0.33 / 2.0, 0.66 / 2.0, 0.99 / 2.0])
+        );
         u /= 3.0;
         assert_eq!(
-            [to_p7(u[0]), to_p7(u[1]), to_p7(u[2])],
-            [to_p7(5.0 / 3.0), to_p7(10.0 / 3.0), to_p7(15.0 / 3.0)]
+            to_3p7([u[0], u[1], u[2]]),
+            to_3p7([0.33 / 6.0, 0.66 / 6.0, 0.99 / 6.0])
         );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,32 @@
+use raytracer_ow::Color;
 use raytracer_ow::PpmWriter;
+//use raytracer_ow::Vec3;
 use std::error::Error;
 use std::fs::File;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let data: Vec<[u8; 3]> = vec![
-        [255, 0, 0],
-        [0, 255, 0],
-        [0, 0, 255],
-        [255, 255, 0],
-        [255, 255, 255],
-        [0, 0, 0],
-    ];
-
-    let file = File::create("test.ppm")?;
+fn gen_ppm(path: &str, data: &Vec<Color>, width: u32, height: u32) -> Result<(), Box<dyn Error>> {
+    let data = data.iter().map(|color| color.to_bytes()).collect();
+    let file = File::create(path)?;
     let mut ppmstream = PpmWriter::new(file);
+    ppmstream.write(data, width, height, 255)?;
+    Ok(())
+}
+fn main() -> Result<(), Box<dyn Error>> {
+    let width = 256;
+    let height = 256;
 
-    ppmstream.write(data, 3, 2, 255)?;
+    let mut data: Vec<Color> = Vec::new();
+    for row in 0..height {
+        for col in 0..width {
+            data.push(Color::new(
+                (col as f64) / (width as f64 - 1.0),
+                (row as f64) / (height as f64 - 1.0),
+                0.0,
+            ));
+        }
+    }
+
+    gen_ppm("test.ppm", &data, width, height)?;
 
     Ok(())
 }
